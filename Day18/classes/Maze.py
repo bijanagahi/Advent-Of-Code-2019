96884a81988090177828901d037230d9f6b44ef3
Bijan Agahi <bijanagahi@yahoo.com>
2019-12-22 13:17:03 -0700

    More work on the Maze class and BFS

--- Day18/classes/Maze.py
+++ Day18/classes/Maze.py
@@ -1,24 +1,74 @@
+from classes.Node import Node
+import random
+
 class Maze(object):
-    """docstring for Maze"""
+    """Maze class. Holds a 1D array reperesenting the maze"""
     def __init__(self, width, height):
-        # self.grid = [[None for i in range(sizeX)] for j in range(sizeY)]
         self.grid = [None] * (width*height)
         self.width = width
         self.height = height
+        self.deltas = [(1,0),(-1,0),(0,1),(0,-1)]
+        self.nodes = {}
+        self.keys = []
+        self.doors = []
 
     def addNode(self, node):
         self.grid[self.c2i(node.x, node.y)] = node
+        self.nodes[(node.x,node.y)] = node
 
+    # Convert 1D index to 2D coordinate
     def i2c(self, i):
         x = i%self.width
         y = i//self.width
         return (x,y)
 
+    # Convert 2D coordinate to 1D index
     def c2i(self,x,y):
         return x + self.width*y
 
+    def bfs(self, start):
+        assert isinstance(start, tuple), "BFS requires a tuple as a starting coordinate"
+        start = self.nodes[start]
+        stack = [start]
+        self.markVisited(start)
+        while(stack):
+            self.addToStack(stack, stack.pop())
+
+    def addToStack(self,stack,node):
+        random.shuffle(self.deltas) # for funzies
+        for delta in self.deltas:
+             if (node.x + delta[0], node.y + delta[1]) in self.nodes:
+                newNode = self.nodes[(node.x + delta[0], node.y + delta[1])]
+                if self.legalNode(newNode):
+                    stack.append(newNode)
+                    self.markVisited(newNode)
+
+    
+    def legalNode(self, node):
+        # If it's a wall, it's not a legal node.
+        if node.type == "WALL" or node.visited:
+            return False
+        # Paths are fine
+        if node.type == 'PATH':
+            return True
+        # Here's the interesting one, doors. We need to see if we have the key first
+        if node.type == 'DOOR':
+            if node.value.lower() in self.keys:
+                return True
+            else:
+                return False
+        return True
+
+
+    
+    def markVisited(self, node):
+        if node.type == 'KEY':
+            self.keys.append(node.value)
+        if node.type == 'DOOR':
+            return self.doors.append(node.value)
+        node.visited = True
+
     def __str__(self):
-        # return '\n'.join([''.join([str(y) for y in x]) for x in self.grid])
         r = ''
         for i in range(len(self.grid)):
             if i%self.width == 0:
--- Day18/day18_part1.py
+++ Day18/day18_part1.py
@@ -2,36 +2,15 @@ import sys
 from classes.Node import Node
 from classes.Maze import Maze
 
-def bfs(grid, pt):
-  stack = [pt]
-  while(stack):
-    addToStack(grid, stack, stack.pop())
-
-deltas = [(1,0),(-1,0),(0,1),(0,-1)]
-def addToStack(grid, stack, pt):
-  for delta in deltas:
-    newPt = addPts(pt, delta)
-    if inBounds(grid, newPt) and legalChar(grid, newPt):
-      stack.append(newPt)
-      markVisited(grid, newPt)
-
-def addPts(pt1, pt2):
-  return (pt1[0] + pt2[0], pt1[1] + pt2[1])
-
-def inBounds(grid, pt):
-  return (0 <= pt[0] < len(grid)) and (0 <= pt[1] < len(grid[0]))
-
-def legalChar(grid, pt):
-  return grid[pt[0]][pt[1]] != "#"
-
-def markVisited(grid, pt):
-  grid[pt[0]][pt[1]] = "#"
+# Day 18 Solution by Bijan Agahi and Josh Lund #
 
 def parse(f):
   grid = []
   for line in f.readlines():
+    if '@' in line:
+        entrance = (line.find('@'),len(grid))
     grid.append([char for char in line.strip()])
-  return grid
+  return (grid, entrance)
        
 
 # def parseInput(puzzle_input):
@@ -44,22 +23,24 @@ def parse(f):
 #     return maze,entrance
 
 
-def main(grid):
-	print('\n'.join([''.join([y for y in x]) for x in grid]))
-	sizeX = len(grid[0])
-	sizeY = len(grid)
-	print(f"Width: {sizeX}, Height: {sizeY}")
-	maze = Maze(sizeX, sizeY)
-	for i in range(len(grid)):
-		for j in range(len(grid[i])):
-			n = Node(j,i,grid[i][j])
-			maze.addNode(n)
-	print(maze)
-	i = maze.c2i(0,2)
-	print(i,maze.grid[i])	
+def main(parsed_input):
+    # print('\n'.join([''.join([y for y in x]) for x in grid]))
+    grid, entrance = parsed_input
+    print(entrance)
+    sizeX = len(grid[0])
+    sizeY = len(grid)
+    print(f"Width: {sizeX}, Height: {sizeY}")
+    maze = Maze(sizeX, sizeY)
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            n = Node(x,y,grid[y][x])
+            maze.addNode(n)
+    print(maze)   
+    # print(grid[1][1])
     # bfs(grid,(1,1))
-    # print(grid)
-
+    # print('\n'.join([''.join([y for y in x]) for x in grid]))
+    maze.bfs(entrance)
+    print([str(x) for x in maze.keys])
 
 
 if __name__ == '__main__':
